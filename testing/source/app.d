0037726f261a344dae9fc156165acc61175cbbc4
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-03-17 18:41:41 +0200

    Testing
    - Added testing code

---
+++ testing/source/app.d
@@ -0,0 +1,57 @@
+import bcm2835;
+import std.stdio : writeln;
+import core.thread : Thread, dur, Duration;
+
+void main()
+{
+	writeln("Startup");
+
+	// initialize the driver (this may throw an exception)
+	auto driver = new BCM2835();
+	writeln("Got driver: ", driver);
+
+	// set the pins as outputs
+	Pin pin_1 = 18;
+	Pin pin_2 = 23;
+	Pin pin_3 = 24;
+	driver.output(pin_1, pin_2, pin_3);
+
+	// drive them all low
+	driver.low(pin_1);
+	driver.low(pin_2);
+	driver.low(pin_3);
+
+	driver.setState(pin_1, pin_2, pin_3, State.Low);
+	Thread.sleep(dur!("seconds")(1));
+	driver.setState(pin_1, pin_2, pin_3, State.High);
+	Thread.sleep(dur!("seconds")(1));
+	driver.setState(pin_1, pin_2, pin_3, State.Low);
+	Thread.sleep(dur!("seconds")(1));
+
+	// do a fancy little light affect
+	Duration wait = dur!("msecs")(100);
+	while(true)
+	{
+		writeln("red");
+		
+		driver.setState(pin_1, State.High);
+		driver.setState(pin_2, State.Low);
+		driver.setState(pin_3, State.Low);
+
+		Thread.sleep(wait);
+
+		writeln("blue");
+		driver.setState(pin_1, State.Low);
+		driver.setState(pin_2, State.High);
+		driver.setState(pin_3, State.Low);
+
+		Thread.sleep(wait);
+
+		writeln("green");
+		driver.setState(pin_1, State.Low);
+		driver.setState(pin_2, State.Low);
+		driver.setState(pin_3, State.High);
+		
+		Thread.sleep(wait);
+	}
+}
