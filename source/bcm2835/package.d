de1847e9c3b1d9fedeec1334075a08e8ac05874e
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-03-17 18:01:41 +0200

    bcm2835 (package)
    - Added

---
+++ source/bcm2835/package.d
@@ -0,0 +1,14 @@
+/**
+ * bcm2835-d
+ */
+module bcm2835;
+
+/**
+ * Driver and exception type
+ */
+public import bcm2835.driver : BCM2835, BCM2835Exception;
+
+/**
+ * Type definitions
+ */
+public import bcm2835.types: Pin, Mode, State;
